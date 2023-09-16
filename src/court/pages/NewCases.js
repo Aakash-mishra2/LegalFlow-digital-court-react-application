6219c8b893fb5af3a6c1f86c89806ef7c210bb20
Aakash Mishra <aakashmishra.281@gmail.com>
2023-09-17 00:07:42 +0530

    Register new case application done

--- src/court/pages/NewCases.js
+++ src/court/pages/NewCases.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { useForm } from "../../shared/hooks/form-hook";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
@@ -11,17 +11,19 @@ import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../../shared/util/valida
 
 import './CasesForm.css';
 import '../components/styles/CaseItem.css';
+import ErrorModal from "../../shared/UIelements/ErrorModal";
+import LoadingSpinner from "../../shared/UIelements/LoadingSpinner";
 
 export default function NewCases() {
     const currentUserId = useSelector((state) => state.userAccount.UserId);
     const currentUserName = useSelector((state) => state.userAccount.userName);
-    const [ confirmCase , setConfirmCase ] = useState(false);
-    const [ isDescBox, setIsBox ] = useState(false);
+    const [confirmCase, setConfirmCase] = useState(false);
+    const [isDescBox, setIsBox] = useState(false);
     const openDescBox = () => { setIsBox(true); }
     const closeDescBox = () => { 
-        setConfirmCase(false);    
-        setIsBox(false); 
-    }
+        setError(' Application Not Submitted! Recheck your Application and ADD CASE again.')
+        setConfirmCase(false); 
+        setIsBox(false); }
 
     const [formState, inputHandler] = useForm({
         aadhar_no: {
@@ -41,12 +43,16 @@ export default function NewCases() {
     const caseConfirmation = () => {
         setConfirmCase(prevMode => !prevMode);
     }
-    
-    const [ newCase , setNewCase ] = useState({
+
+    const [newCase, setNewCase] = useState({
         court: '', description: '', judge: ''
     });
 
-    const  caseSubmitHandler = async (event) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    const history = useNavigate();
+
+    const caseSubmitHandler = async (event) => {
         event.preventDefault();
         setNewCase({
             court: formState.inputs.courtName.value,
@@ -58,29 +64,45 @@ export default function NewCases() {
         });
         openDescBox();
         caseConfirmation();
-        // try{
-        //     const response = api.post('/admin/newCase', newCase);
-
-        // }
-        // catch(err){
-
-        // }
     };
+    const clearError = () => {
+        setError(null);
+    }
+    const submitApplication = async () => {
+        caseConfirmation();
+        setIsLoading(true);
+        try {
+            const response = await api.post('/admin/newcase', newCase);
+            console.log(response.data.added_NewCase);
+            setIsLoading(false);
+        }
+        catch (err) {
+            setIsLoading(false);
+            if (err.response) {
+                setError(err.response.data.message);
+                console.log(err.response.status);
+                console.log(error);
+            } else {
+                setError(err.message);
+            }
+        }
+        
+    }
 
-    return (
-        <React.Fragment>
+return (
+    <React.Fragment>
         <Modal
-            show = {isDescBox && confirmCase}
-            closeBox = {closeDescBox}
-            header = "Confirm Your New Case Application "
+            show={isDescBox && confirmCase}
+            closeBox={closeDescBox}
+            header="Confirm Your New Case Application "
             contentClass="case-item__modal-content"
             footerClass="case-item__modal-actions"
-            footer = {
+            footer={
                 <span>
-                <p style={{fontSize : 13, textAlign: "left" }}> I hereby confirm all above details for my new case application.
-                 Correctness of all details while verification is my responsibility. </p>
-                <Button onClick = {caseConfirmation}>SUBMIT</Button>
-                <Button danger onClick={closeDescBox}>GO BACK </Button>
+                    <p style={{ fontSize: 13, textAlign: "left" }}> I hereby confirm all above details for my new case application.
+                        Correctness of all details while verification is my responsibility. </p>
+                    <Button onClick={submitApplication}>SUBMIT</Button>
+                    <Button danger onClick={closeDescBox}>GO BACK </Button>
                 </span>
             }
         >
@@ -90,22 +112,23 @@ export default function NewCases() {
             <h4><b>Description : </b><em>{newCase.description}</em></h4>
             <p><b>Judge : </b><em>{newCase.judge}</em></p>
         </Modal>
-
+        {isLoading && <LoadingSpinner asOverlay />}
+        <ErrorModal error={error} onClear={clearError} />
         <form className="case-form" onSubmit={caseSubmitHandler}>
             <Input
+                id="aadhar_no"
                 element="input"
                 type="text"
-                id="aadhar_no"
-                label="ID no. "
+                label="Your AADHAR/ID card no. "
                 placeHolder="Enter your Aadhar Card NO. / Voter ID no. "
                 errorText="Please Enter a valid no. "
                 validators={[VALIDATOR_MINLENGTH(12)]}
                 onInput={inputHandler}
             />
             <Input
+                id="courtName"
                 element="textarea"
                 type="text"
-                id="courtName"
                 label="Court Name"
                 placeHolder="Name of your Court"
                 errorText="This is a required field. "
@@ -113,18 +136,17 @@ export default function NewCases() {
                 onInput={inputHandler}
             />
             <Input
+                id="caseDesc"
                 element="textarea"
                 type="text"
-                id="caseDesc"
                 label="Case Description"
                 placeHolder=" Brief Summary of your case Application (200 words )."
-                errorText="This is a required field. "
-                validators={[VALIDATOR_REQUIRE()]}
+                errorText="This is a required field! Minimum 10 words. "
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(10)]}
                 onInput={inputHandler}
             />
-
             <Button type="submit" disabled={!formState.isValid}>ADD CASE</Button>
         </form>
-        </React.Fragment>
-    );
+    </React.Fragment>
+);
 }
