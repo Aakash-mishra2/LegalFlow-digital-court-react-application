6bda87bd72322ebfe922e7d6cba7029ce4a450e4
sky-dev <aakashmishra.281@gmail.com>
2025-08-07 04:27:27 +0530

    resolve inaccesible routes error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Sidebar from "./shared/Navigation/Sidebar";
 import LoadingSpinner from "./components/LoadingSpinner";
@@ -13,32 +13,34 @@ import NewCases from "./court/components/NewCase/NewCases";
 import Authenticate from "./components/dashboard/Authenticate";
 
 const App = () => {
-  const isloggedIn = !!localStorage.getItem('Access-token');
-  // Keep user on same route after reload if logged in
-  useEffect(() => {
-    if (isloggedIn) {
-      const currentPath = window.location.pathname;
-      // If user is on /auth or /, redirect to /dashboard
-      if (currentPath === '/auth' || currentPath === '/') {
-        window.history.replaceState({}, '', '/dashboard');
-      }
-    }
-    else if (!localStorage.getItem('Access-token')) {
-      const currentPath = window.location.pathname;
-      // If user is on /auth or /, redirect to /dashboard
-      if (currentPath === '/auth' || currentPath === '/') {
-        window.history.replaceState({}, '.');
-      }
-    }
-  }, [isloggedIn]);
+
+  const [isloggedIn, setIsloggedIn] = useState(localStorage.getItem("isLoggedIn") === "true");
 
   useEffect(() => {
     const handleStorage = () => {
-      //setIsloggedIn(!!localStorage.getItem('Access-token'));
+      setIsloggedIn(localStorage.getItem("isLoggedIn") === "true");
     };
     window.addEventListener('storage', handleStorage);
     return () => window.removeEventListener('storage', handleStorage);
   }, []);
+
+  //On initial Load (home screen), set isLoggedIn to false
+  useEffect(() => {
+    if (window.location.pathname === "/" || window.location.pathname === "/auth") {
+      localStorage.setItem("isLoggedIn", "false");
+    }
+  }, []);
+
+  // Keep user on same route after reload if logged in
+  useEffect(() => {
+    if (isloggedIn) {
+      const currentPath = window.location.pathname;
+      if (currentPath === '/auth' || currentPath === '/') {
+        window.history.replaceState({}, '', '/dashboard');
+      }
+    }
+  }, [isloggedIn]);
+
 
   let routes;
   if (isloggedIn) {
@@ -58,7 +60,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Authenticate />} />
         <Route path="/contact-us" element={<ContactUsForm />} />
-        <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     );
   }
